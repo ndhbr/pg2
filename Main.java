2368b31ffad1e1d43c734e2270426ff6c6b01450
Andreas Huber <andi@pl32.de>
2019-03-21 11:01:54 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main 
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World!");
+        test();
+    }
+
+    public static void test()
+    {
+        String a = "lol";
+        System.out.println("Test");
+    }
+}
